4d41908069f24b261fab15ee7aa3ccd940e183d7
(no author) <(no author)@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2003-09-21 02:07:08 +0000

    This commit was manufactured by cvs2svn to create tag 'version_0-9-26'.
    git-svn-id: http://svn.boost.org/svn/boost/tags/version_0-9-26@20128 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/tools/inspect/wrong_line_ends_test.cpp
+++
@@ -1,7 +0,0 @@
-// this is a test file; it isn't supposed to have correct line endings
-
-//  Copyright Beman Dawes, 2003.
-//  See accompanying license for terms and conditions of use.
-
-line ending with cr
-line ending without any cr or nl
