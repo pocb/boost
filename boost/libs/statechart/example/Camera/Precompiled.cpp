4eee359c8fbed3ca670a7f054976d9d4121db256
(no author) <(no author)@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2004-10-20 08:26:43 +0000

    This commit was manufactured by cvs2svn to create branch 'RC_1_32_0'.
    git-svn-id: http://svn.boost.org/svn/boost/branches/RC_1_32_0@25797 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- boost/libs/statechart/example/Camera/Precompiled.cpp
+++
@@ -1,9 +0,0 @@
-//////////////////////////////////////////////////////////////////////////////
-// (c) Copyright Andreas Huber Doenni 2002-2004
-// Distributed under the Boost Software License, Version 1.0. (See accompany-
-// ing file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//////////////////////////////////////////////////////////////////////////////
-
-
-
-#include "Precompiled.hpp"
