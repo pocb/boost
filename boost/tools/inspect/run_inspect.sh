77e189fff5f844c11f319010e62e4acd176ad99f
grafik <grafik@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2006-07-15 23:32:07 +0000

    Merge from HEAD.
    git-svn-id: http://svn.boost.org/svn/boost/branches/RC_1_34_0@34544 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/tools/inspect/run_inspect.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+#~ Copyright Rene Rivera 2006.
+#~ Distributed under the Boost Software License, Version 1.0.
+#~ (See accompanying file LICENSE_1_0.txt or copy at
+#~ http://www.boost.org/LICENSE_1_0.txt)
+
+set -e
+
+#~ Configuration options.
+mail_to="Rene Rivera <grafikrobot@gmail.com>"
+#~ mail_to="Boost <boost@lists.boost.org>"
+cvs_branch="$1"
+cvs_user=":ext:${USER}"
+cvs_co="cvs -q -z9 -d${cvs_user}@boost.cvs.sourceforge.net:/cvsroot/boost co -P -r ${cvs_branch}"
+cvs_dir="${HOME}/devroots"
+
+#~ Build bjam.
+cd ${cvs_dir}
+${cvs_co} -d boost_jam_src boost/tools/jam/src
+cd boost_jam_src
+LOCATE_TARGET=bin sh ./build.sh
+
+#~ Build inspect.
+cd ${cvs_dir}
+${cvs_co} -d boost_${cvs_branch} boost
+cd boost_${cvs_branch}/tools/inspect/build
+${cvs_dir}/boost_jam_src/bin/bjam --v2
+
+#~ Run the inspection.
+cd ${cvs_dir}
+cd boost_${cvs_branch}
+./dist/bin/inspect -text -license -copyright -crlf -link -long_name -tab -minmax > inspect-out.txt
+
+#~ Send email with results.
+mail_date=`date --iso-8601 --utc`
+/usr/sbin/sendmail "${mail_to}" <<EMAIL
+From: Rene Rivera <grafikrobot@gmail.com>
+To: ${mail_to}
+Reply-To: Boost <boost@lists.boost.org>
+Subject: Boost inspection notification (${mail_date}/${cvs_branch})
+
+`cat inspect-out.txt`
+EMAIL
