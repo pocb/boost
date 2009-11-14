e529ac5fbb329a8902a8f5a6da5d71f06d2eae24
troy d. straszheim <troy@resophonic.com>
2009-11-14 11:03:36 -0500

    Be specific about 'git' so's automatic testing works

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,8 +7,11 @@
 set(CTEST_PROJECT_NAME "Boost")
 set(CTEST_NIGHTLY_START_TIME "03:00:00 EST")
 
+set(CTEST_UPDATE_TYPE "git")
+
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=Boost")
 set(CTEST_DROP_SITE_CDASH TRUE)
-set(CTEST_UPDATE_COMMAND "HI")
+
+set(UPDATE_COMMAND "git")
