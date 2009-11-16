d8dedb593ac001e7fa2e4205865268d3390b1fb0
troy d. straszheim <troy@resophonic.com>
2009-11-16 09:46:54 -0500

    dart testing tweaks

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,14 +1,22 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
+
+message("--------------- CTestConfig.cmake --------------")
 set(CTEST_PROJECT_NAME "Boost")
 set(CTEST_NIGHTLY_START_TIME "03:00:00 EST")
+
+set(CTEST_UPDATE_TYPE "git")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=Boost")
 set(CTEST_DROP_SITE_CDASH TRUE)
-set(CTEST_UPDATE_COMMAND "HI")
+
+#
+#  You're welcome to set this to 
+#
+#set(BUILD_PROJECTS "crc")
+set(BUILD_TESTS "ALL")
+
+
---
+++ DartConfig.cmake
@@ -0,0 +1,7 @@
+message("-------------- src/DartConfig.cmake ------------------")
+# set(CTEST_UPDATE_COMMAND "git pull")
+
+set(UPDATE_COMMAND "git")
+set(SITE "zinc.dc.resophonic.com")
+set(CMAKE_VERBOSE_MAKEFILE TRUE)
+
