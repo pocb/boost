959507f0c70856577fa0ed3f04aebfd46f3461eb
troy d. straszheim <troy@resophonic.com>
2009-11-17 11:52:58 -0500

    don't need this

--- DartConfig.cmake
+++
@@ -1,7 +0,0 @@
-message("-------------- src/DartConfig.cmake ------------------")
-# set(CTEST_UPDATE_COMMAND "git pull")
-
-set(UPDATE_COMMAND "git")
-set(SITE "zinc.dc.resophonic.com")
-set(CMAKE_VERBOSE_MAKEFILE TRUE)
-
