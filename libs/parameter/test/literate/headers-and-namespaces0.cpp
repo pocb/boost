bd00cf37df291972097f535be30fafbed8f03582
danieljames <danieljames@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-11-01 13:02:52 +0000

    Quickbook: Remove boost libs from dev branch, will replace with new versions.
    Hopefully, this will allow a merge without having to upload every change since
    I branched.
    
    
    git-svn-id: http://svn.boost.org/svn/boost/branches/quickbook-dev@75211 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/parameter/test/literate/headers-and-namespaces0.cpp
+++
@@ -1,5 +0,0 @@
-#line 274 "../../../../libs/parameter/doc/index.rst"
-#include <boost/parameter/keyword.hpp>
-#line 283 "../../../../libs/parameter/doc/index.rst"
-using boost::parameter::keyword;
-
